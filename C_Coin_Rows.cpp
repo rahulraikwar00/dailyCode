316c5337b0a4e7d948dfe01ebde32cb00d4f3bcf
rahul <rr200636@gmail.com>
2021-08-05 05:50:02 +0530

    C_codin_row.cpp add

--- C_Coin_Rows.cpp
+++ C_Coin_Rows.cpp
@@ -1,40 +1,70 @@
 #include <iostream>
 using namespace std;
-int solve(int count, int &arr, int i, int j)
-{
-    if (arr[i][j] == 0)
-    {
-        return count;
-    }
-    count = solve(count, arr, i, j + 1) + solve(count, arr, i, j + 1);
-    return count;
-}
-
 int main()
 {
     int t, n;
     cin >> t;
     while (t--)
     {
-        cin >> n;anf("%lld", &arr[i][j]);
-
-                // arr[ii][i] += ps[ii][i - 1];
-            }
-        int arr[3][n + 1];
-        for (int i = 0; i <= 2; i++)
+        int a;
+        cin >> a;
+        int arr[2][a + 1];
+        int count = 0;
+        int temp = 0;
+        if (a == 1)
         {
-            for (int j = 0; j <= n; j++)
-            {
-                cin >> arr[i][j];
-                if (i == 0 | j == 0)
-                {
-                    arr[i][j] = 0;
-                }
-
-                // scanf("%lld", &arr[i][j]);
-
-                // arr[ii][i] += ps[ii][i - 1];
-            }
+            cout << 0 << endl;
+            continue;
         }
-        cout << solve(0, arr, 0, 0);
+        for (int j = 1; j < a + 1; j++)
+        {
+            cin >> temp;
+            count += temp;
+            arr[0][j] = count;
+            temp = 0;
+        }
+        count = 0;
+        for (int j = 1; j < a + 1; j++)
+        {
+            cin >> temp;
+            count += temp;
+            arr[1][j] = count;
+            temp = 0;
+        }
+
+        arr[0][0] = 0;
+        arr[1][0] = 0;
+        int ans = 1e9;
+        for (int i = 1; i < a + 1; i++)
+        {
+
+            ans = min(ans, max(arr[1][i - 1], arr[0][a] - arr[0][i]));
+        }
+
+        cout << ans << endl;
     }
+}
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int t;int n,a[100010],b[100010];
+// int main(){
+// 	cin>>t;
+// 	while(t--){
+// 		cin>>n;
+// 		for(int i=1;i<=n;i++){
+// 			cin>>a[i];
+// 			a[i]+=a[i-1];
+// 		}
+// 		for(int i=1;i<=n;i++){
+// 			cin>>b[i];
+// 			b[i]+=b[i-1];
+// 		}
+// 		int ans=0x7fffffff;
+// 		for(int i=1;i<=n;i++){
+// 			ans=min(ans,max(a[n]-a[i],b[i-1]));
+// 		}
+// 		cout<<ans<<endl;
+// 	}
+// }

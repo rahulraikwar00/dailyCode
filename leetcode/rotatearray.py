b39e3c565611b4713485e4194a69f26c89b72de9
rahul <rr200636@gmail.com>
2022-01-02 20:49:56 +0530

    rotatearray.py

---
+++ leetcode/rotatearray.py
@@ -0,0 +1,12 @@
+#Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[-k:] + nums[:-k]
+        return nums
